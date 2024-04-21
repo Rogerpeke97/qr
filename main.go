fb49d904d940ea39831f677ba6e7a94a3c5a0937
Ignacio <rogerpeke97@gmail.com>
2024-04-20 23:52:14 -0400

    starting encoding for the qr gen

--- main.go
+++ main.go
@@ -5,17 +5,21 @@ import (
 	"strconv"
 )
 
+var power_of_code = byte('^')
+var x_code = byte('x')
+var ascii_integer_min_max = []int{48, 57}
+
+// Encoded data starts with the mode
+var BYTE_MODE_INDICATOR = "0100"
+
 // x and degree
 func findMultiplierOfHighestDegree(
 	f string,
 ) (string, int) {
-	power_of_code := byte('^')
-	x_code := byte('x')
 	highest_degree_f := 0
 	highest_degree_f_x := ""
-	ascii_integer_min_max := []int{48, 57}
-
 	f_len := len(f)
+
 	for i := 0; i < f_len; i++ {
 		if f[i] == x_code {
 			if i+1 < f_len && f[i+1] == power_of_code && i+2 < f_len {
@@ -66,41 +70,59 @@ func findMultiplierOfHighestDegree(
 	return highest_degree_f_x, highest_degree_f
 }
 
-func findGcd(
-	f string,
-	g string,
-	fp uint,
-) string {
+// Error correction level L just to keep it simple for now
+// I'll add more later if not lazy :)
+// Version will be 4 = 33x33 pixels
+// The amount of characters using byte encoding is 78
+// char count must be 8 bits in byte mode for versions 1...9
+// returns mode, char_count_indicator, encoded_data
+func encode(
+	str string,
+) (string, string, string) {
+	char_count_max_bit_long := 8
+	char_count := int64(len(str))
+	// Goes after mode indicator
+	char_count_bin := strconv.FormatInt(char_count, 2)
+	char_count_bin_len := len(char_count_bin)
+	if char_count_bin_len > char_count_max_bit_long {
+		fmt.Printf("\nMax bits for char count exceeded. Need %d, got %d\n", char_count_max_bit_long, len(char_count_bin))
+		panic("Failed!")
+	}
 
-}
+	if char_count_bin_len < char_count_max_bit_long {
+		padding := char_count_max_bit_long - char_count_bin_len
+		var pad_zeros string
+		for i := 0; i < padding; i++ {
+			pad_zeros += "0"
+		}
 
-func encoder() {
-	// url_to_encode := "https://github.com/Rogerpeke97"
-	url_to_encode := "AB"
-	var bin_representation [][]int
-	encoded := []byte(url_to_encode)
+		char_count_bin = pad_zeros + char_count_bin
+	}
+
+	var data_bin_str string
+	encoded := []byte(str)
 	for i := 0; i < len(encoded); i++ {
-		bin_representation = append(bin_representation, []int{})
-		// big endian so opposite
+		// assumes machine stores bin in little endian
 		for j := 7; j >= 0; j-- {
 			mask := byte(1 << uint(j))
 			bin := encoded[i] & mask
-			var bin_to_add int
+			var bin_str_to_add string
 			if bin > 0 {
-				bin_to_add = 1
+				bin_str_to_add = "1"
 			} else {
-				bin_to_add = 0
+				bin_str_to_add = "0"
 			}
 
-			bin_representation[i] = append(bin_representation[i], bin_to_add)
+			data_bin_str += bin_str_to_add
 		}
 	}
 
-	fmt.Printf("\nBin representation is: %+v\n", bin_representation)
+	fmt.Printf("\nBin representation is: %s\n", data_bin_str)
+	return BYTE_MODE_INDICATOR, char_count_bin, data_bin_str
 }
 
 func main() {
-	// f := "x^3 - 8"
-	// g := "3x^2 - 6x"
-
+	str := "HELLO WORLD"
+	mode, char_count_indicator, data := encode(str)
+	fmt.Printf("\nMode is: %s\nChar count is: %s\nData is: %s\n", mode, char_count_indicator, data)
 }
